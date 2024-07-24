b363ad20874cc6228b2d12441e85749bb52d4705
박지윤 <tkrhkfld123@naver.com>
2024-07-24 19:52:48 +0900

    chore: 콘솔 제거

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -4,8 +4,6 @@ const KakaoLogin = () => {
   const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
   const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-  const code = new URL(window.location.href).searchParams.get('code');
-  console.log(code);
 
   const handleLogin = () => {
     window.location.href = kakaoURL;
