8441744827cec2777ea7265c53a5dd449d8fa224
박지윤 <tkrhkfld123@naver.com>
2024-07-24 19:16:54 +0900

    feat: kakao 소셜 로그인 구현

---
+++ src/components/auth/KakaoLogin.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const KakaoLogin = () => {
+  const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  const code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+
+  return <button onClick={handleLogin}>카카오 로그인</button>;
+};
+
+export default KakaoLogin;
