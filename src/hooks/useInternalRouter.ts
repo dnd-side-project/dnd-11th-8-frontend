ceeee9e0b8b6e6a5f056a8beaf728226aad7d037
박지윤 <tkrhkfld123@naver.com>
2024-07-24 18:04:17 +0900

    feat: internalrouter 훅 구현하기

---
+++ src/hooks/useInternalRouter.ts
@@ -0,0 +1,32 @@
+import { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type RoutePath =
+  | '/'
+  | '/onboarding'
+  | '/login'
+  | '/guide'
+  | '/my-plant'
+  | '/my-plant/add'
+  | '/profile'
+  | '/profile/edit';
+
+const useInternalRouter = () => {
+  const navigate = useNavigate();
+
+  return useMemo(() => {
+    return {
+      goBack() {
+        navigate(-1);
+      },
+      push(path: RoutePath) {
+        navigate(path);
+      },
+      replace(path: RoutePath) {
+        navigate(path, { replace: true });
+      },
+    };
+  }, [navigate]);
+};
+
+export default useInternalRouter;
