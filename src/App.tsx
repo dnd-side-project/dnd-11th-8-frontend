cf8c631c97ab55af8406c842b10f146469debff1
chajuhui123 <chajuhui123@naver.com>
2024-07-25 23:50:03 +0900

    chore: test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AppleLogin from './components/auth/AppleLogin';
 import KakaoLogin from './components/auth/KakaoLogin';
 
 function App() {
+  console.log('test');
   return (
     <div>
       <div className="bg-red-400">화이팅 ~</div>
