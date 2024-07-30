3995da0cc6bb7a36149c5cd6a2df29387bb57bf8
박지윤 <tkrhkfld123@naver.com>
2024-07-31 00:31:27 +0900

    feat: list 컴포넌트 구현 완료

--- src/components/common/List/List.stories.tsx
+++ src/components/common/List/List.stories.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import ListComponent from './index';
+
+const items = [
+  {
+    id: 1,
+    name: 'Input1',
+  },
+  {
+    id: 2,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhKx2Fwr5Y8BEdj-CjHu5beloCcpqRVg6uJpTK6yH9vtSz_I9o',
+    name: 'Input2',
+  },
+  {
+    id: 3,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhKx2Fwr5Y8BEdj-CjHu5beloCcpqRVg6uJpTK6yH9vtSz_I9o',
+    name: 'Input3',
+    badges: ['뱃지S', '뱃지S', '뱃지S'],
+  },
+];
 
 const meta: Meta = {
   title: 'ListComponent',
   component: ListComponent,
   render: () => (
     <div className="w-screen h-screen">
-      <ListComponent />
+      <div className="w-[370px]">
+        {items.map((item) => (
+          <ListComponent
+            key={item.id}
+            image={item.image}
+            name={item.name}
+            badges={item.badges}
+            onChange={() => alert('해당 식물로 페이지 이동 ~')}
+          />
+        ))}
+      </div>
     </div>
   ),
 };
