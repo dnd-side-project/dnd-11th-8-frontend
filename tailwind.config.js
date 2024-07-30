9110e6f46999394309bb21b732d814d8a3b96de2
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-07-30 13:27:02 +0900

    config: tailwind 폰트 사이즈 테마 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      'sub-typo': ['11px', '16.5px'],
+      'small-writing': ['13px', '19.5px'],
+      'small-body': ['17px', '25.5px'],
+      'small-title': ['20px', '29px'],
+      'regular-title': ['22px', '31px'],
+      'large-title': ['26px', '35px'],
+      'extra-large-title': ['30px', '40px'],
+    },
   },
   plugins: [],
 };
