bfc1d898b922c90794fc7d25b490d088004c7304
박지윤 <tkrhkfld123@naver.com>
2024-07-30 22:31:33 +0900

    feat: toggle 기능 구현 완료

--- src/components/common/Toggle.tsx
+++ src/components/common/Toggle.tsx
@@ -1,10 +1,29 @@
 import { Switch } from '@/components/ui/switch';
+import { FormField } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
 
-const Toggle = () => {
+interface ToggleProps {
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ checked, onCheckedChange }) => {
+  const form = useForm();
+
   return (
-    <>
-      <Switch></Switch>
-    </>
+    <FormField
+      control={form.control}
+      name="toggleButton"
+      render={({ field }) => {
+        const handleCheckedChange = (value: boolean) => {
+          if (onCheckedChange) {
+            onCheckedChange(value);
+          }
+          field.onChange(value);
+        };
+        return <Switch checked={checked} onCheckedChange={handleCheckedChange} />;
+      }}
+    />
   );
 };
 
