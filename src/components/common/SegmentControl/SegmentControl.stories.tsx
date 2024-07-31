2bcaea934f2188e4f674418188f9aaf56ba7da0f
박지윤 <tkrhkfld123@naver.com>
2024-07-31 20:00:11 +0900

    feat: 세그먼트 컨트롤 컴포넌트 구현 완료

--- src/components/common/SegmentControl/SegmentControl.stories.tsx
+++ src/components/common/SegmentControl/SegmentControl.stories.tsx
@@ -1,12 +1,39 @@
 import { Meta, StoryObj } from '@storybook/react';
 import SegmentControl from './index';
 
+const segments = [
+  {
+    id: 1,
+    name: '베란다',
+  },
+  {
+    id: 2,
+    name: '거실',
+  },
+  {
+    id: 3,
+    name: '루밍이방',
+  },
+  {
+    id: 4,
+    name: '부엌',
+  },
+  {
+    id: 5,
+    name: '사무실',
+  },
+  {
+    id: 6,
+    name: '루밍이방2',
+  },
+];
+
 const meta: Meta = {
   title: 'SegmentControl',
   component: SegmentControl,
   render: () => (
     <div className="w-screen h-screen">
-      <SegmentControl />
+      <SegmentControl segments={segments} />
     </div>
   ),
 };
