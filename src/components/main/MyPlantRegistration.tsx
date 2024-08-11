7f49fbe709f6dc6f8824c80ad92356be7d0f65db
박지윤 <tkrhkfld123@naver.com>
2024-08-11 17:51:25 +0900

    feat: api 명세서대로 구현 완료 및 버튼 이동 구현, 메인 체크버튼 클릭 시 아이콘 색 변경

--- src/components/main/MyPlantRegistration.tsx
+++ src/components/main/MyPlantRegistration.tsx
@@ -1,10 +1,12 @@
+import useInternalRouter from '@/hooks/useInternalRouter';
 import Card from '../common/Card';
 import addIcon from '@/assets/icon/addMyPlant.svg';
 
 const MyPlantRegistration = () => {
+  const { push } = useInternalRouter();
   return (
     <div className="flex justify-center items-center max-w-[331px] mx-auto mt-[19px] mb-[27px]">
-      <div className="w-full">
+      <div className="w-full" onClick={() => push('/my-plant/add')}>
         <Card.Container verticalPaddingSize={'small'}>
           <div className="py-[18px]">
             <Card.Content className={'text-center pb-5'}>
