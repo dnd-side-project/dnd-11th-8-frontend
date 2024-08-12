b4d8483c3fe5cf9c32f5172df69c17ce54cd45aa
박지윤 <tkrhkfld123@naver.com>
2024-08-13 00:33:37 +0900

    feat: 등록된 식물이 없을때 나오는 ui 구현

--- src/pages/MyPlant.tsx
+++ src/pages/MyPlant.tsx
@@ -1,8 +1,8 @@
 import SegmentControl from '@/components/common/SegmentControl';
 import TabBar from '@/components/main/TabBar';
-import MyPlantLayout from '@/components/myPlant/MyPlantLayout';
 import MyPlantList from '@/components/myPlant/MyPlantList';
 import MyPlantSupplement from '@/components/myPlant/MyPlantSupplement';
+import NoMyPlant from '@/components/myPlant/NoMyPlant';
 
 const segments = [
   {
@@ -23,36 +23,45 @@ const segments = [
   },
 ];
 
-const myPlant = [
-  {
-    myPlantId: 1,
-    nickname: '쫑쫑이',
-    scientificName: '몬스테라 델리오사',
-    image:
-      'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5AU5/image/7oHw3DMrgO9_66LHCI3MkoKjY3M.JPG',
-    waterRemainDay: 1,
-    fertilizerRemainDay: 15,
-  },
-  {
-    myPlantId: 2,
-    nickname: '뿡뿡이',
-    scientificName: '병아리눈물',
-    image:
-      'https://thumbnail.10x10.co.kr/webimage/image/basic600/456/B004562843.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false',
-    waterRemainDay: 3,
-    fertilizerRemainDay: 23,
-  },
+interface Plant {
+  myPlantId: number;
+  nickname: string;
+  scientificName: string;
+  image: string;
+  waterRemainDay: number;
+  fertilizerRemainDay: number;
+}
+
+const myPlant: string | Plant[] = [
+  // {
+  //   myPlantId: 1,
+  //   nickname: '쫑쫑이',
+  //   scientificName: '몬스테라 델리오사',
+  //   image:
+  //     'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5AU5/image/7oHw3DMrgO9_66LHCI3MkoKjY3M.JPG',
+  //   waterRemainDay: 1,
+  //   fertilizerRemainDay: 15,
+  // },
+  // {
+  //   myPlantId: 2,
+  //   nickname: '뿡뿡이',
+  //   scientificName: '병아리눈물',
+  //   image:
+  //     'https://thumbnail.10x10.co.kr/webimage/image/basic600/456/B004562843.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false',
+  //   waterRemainDay: 3,
+  //   fertilizerRemainDay: 23,
+  // },
 ];
 
 const MyPlant = () => {
   return (
     <>
-      <MyPlantLayout>
+      <div className="mt-[30px]">
         <SegmentControl segments={segments} />
-        <MyPlantSupplement plants={myPlant} />
-        <MyPlantList plants={myPlant} />
-        <TabBar />
-      </MyPlantLayout>
+      </div>
+      <MyPlantSupplement plants={myPlant} />
+      {myPlant.length === 0 ? <NoMyPlant /> : <MyPlantList plants={myPlant} />}
+      <TabBar />
     </>
   );
 };
