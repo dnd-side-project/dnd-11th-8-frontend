533de0bfbbb69be8c06eab51dfa54e755a2dd541
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-12 19:51:51 +0900

    config: 테일윈드 색상, env 값 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,4 +4,5 @@ interface ImportMetaEnv {
   readonly VITE_KAKAO_REST_API_KEY: string;
   readonly VITE_KAKAO_REDIRECT_URI: string;
   readonly VITE_REDIRECT_URI: string;
+  readonly VITE_NODE_ENV: 'development' | 'production' | 'test';
 }
