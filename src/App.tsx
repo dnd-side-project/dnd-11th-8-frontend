393cec4e6b1f8a84653c7962b06a4f8b7237be99
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-12 20:01:47 +0900

    fix: lint 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import AddPlantPage from './pages/AddPlantPage';
-import SearchPlantPage from '@/pages/SearchPlantPage.tsx';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path={'/my-plant/add'} element={<AddPlantPage />} />
-          <Route path={'/my-plant/search'} element={<SearchPlantPage />} />
         </Routes>
       </BrowserRouter>
     </div>
