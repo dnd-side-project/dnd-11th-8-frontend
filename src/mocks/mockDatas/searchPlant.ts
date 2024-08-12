c948de1baa53aa0acc74fd9d98817f5509d9cc29
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-12 19:52:18 +0900

    config: msw 설정 추가

---
+++ src/mocks/mockDatas/searchPlant.ts
@@ -0,0 +1,82 @@
+export default [
+  {
+    plantId: 1,
+    name: '몬스테라 델리오사',
+  },
+  {
+    plantId: 2,
+    name: '병아리 눈물',
+  },
+  {
+    plantId: 3,
+    name: '스킨답서스',
+  },
+  {
+    plantId: 4,
+    name: '호야 칸니',
+  },
+  {
+    plantId: 5,
+    name: '산세베리아',
+  },
+  {
+    plantId: 6,
+    name: '행운목',
+  },
+  {
+    plantId: 7,
+    name: '알로카시아',
+  },
+  {
+    plantId: 8,
+    name: '칼라데아',
+  },
+  {
+    plantId: 9,
+    name: '파키라',
+  },
+  {
+    plantId: 10,
+    name: '아레카야자',
+  },
+  {
+    plantId: 11,
+    name: '무늬 벵갈 고무나무',
+  },
+  {
+    plantId: 12,
+    name: '페페로미아',
+  },
+  {
+    plantId: 13,
+    name: '금전수',
+  },
+  {
+    plantId: 14,
+    name: '필로덴드론',
+  },
+  {
+    plantId: 15,
+    name: '스파티필름',
+  },
+  {
+    plantId: 16,
+    name: '페르시아 벤자민',
+  },
+  {
+    plantId: 17,
+    name: '드라세나',
+  },
+  {
+    plantId: 18,
+    name: '아이비',
+  },
+  {
+    plantId: 19,
+    name: '비누나무',
+  },
+  {
+    plantId: 20,
+    name: '크루시아 로즈',
+  },
+];
