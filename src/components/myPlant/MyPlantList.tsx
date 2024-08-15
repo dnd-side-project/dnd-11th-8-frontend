8994fb6a39b5ca25d280b50e6b71406119f36e38
박지윤 <tkrhkfld123@naver.com>
2024-08-16 01:36:50 +0900

    chore: 클릭 시 해당 식물로 경로 이동

--- src/components/myPlant/MyPlantList.tsx
+++ src/components/myPlant/MyPlantList.tsx
@@ -1,6 +1,7 @@
 import Badge from '@/components/common/Badge';
 import waterIcon from '@/assets/icon/myPlantWater.svg';
 import fertilizerIcon from '@/assets/icon/myPlantFertilizer.svg';
+import { Link } from 'react-router-dom';
 
 interface MyPlantListProps {
   plants: Array<{
@@ -17,7 +18,11 @@ const MyPlantList: React.FC<MyPlantListProps> = ({ plants }) => {
   return (
     <div>
       {plants.map((plant) => (
-        <div key={plant.myPlantId} className="flex gap-[16px] px-[20px] py-[16px]">
+        <Link
+          to={`/my-Plant/${plant.myPlantId}`}
+          key={plant.myPlantId}
+          className="flex gap-[16px] px-[20px] py-[16px]"
+        >
           <img src={plant.image} alt="내 식물 사진" className="w-[80px] h-[80px] rounded-[10px]" />
           <div className="flex flex-col gap-[8px]">
             <div>
@@ -26,18 +31,18 @@ const MyPlantList: React.FC<MyPlantListProps> = ({ plants }) => {
             </div>
             <div className="flex gap-[5px]">
               <Badge
-                className="flex items-center gap-[5px] px-[8px] py-[4px] bg-Gray50 text-[11px] text-Gray500 font-medium	border border-GrayOpacity100"
+                className="flex items-center gap-[5px] px-[8px] py-[4px] bg-Gray50 text-[11px] text-Gray500 font-medium border border-GrayOpacity100"
                 text={`${plant.waterRemainDay}일 이내`}
                 icon={waterIcon}
               />
               <Badge
-                className="flex items-center gap-[5px] px-[8px] py-[4px] bg-Gray50 text-[11px] text-Gray500 font-medium	border border-GrayOpacity100"
+                className="flex items-center gap-[5px] px-[8px] py-[4px] bg-Gray50 text-[11px] text-Gray500 font-medium border border-GrayOpacity100"
                 text={`${plant.fertilizerRemainDay}일 이내`}
                 icon={fertilizerIcon}
               />
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
