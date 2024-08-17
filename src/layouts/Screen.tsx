7e939aa06853bbbf44084a91a269c6101b3da2d8
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-17 15:44:40 +0900

    fix: className 필수입력이라 발생한 에러 수정

--- src/layouts/Screen.tsx
+++ src/layouts/Screen.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { cn } from '@/utils.ts';
 
-const Screen = ({ children, className }: PropsWithChildren & { className: string }) => {
+const Screen = ({ children, className }: PropsWithChildren & { className?: string }) => {
   return <div className={cn('max-w-md w-full mx-auto px-[24px]', className)}>{children}</div>;
 };
 
