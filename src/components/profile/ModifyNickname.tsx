bf501341c8b9c9c8ea40136a71ee1941e9016aa6
박지윤 <tkrhkfld123@naver.com>
2024-08-19 01:58:49 +0900

    chore: 프로필 페이지 레이아웃 적용 및 텍스트 필드 적용

--- src/components/profile/ModifyNickname.tsx
+++ src/components/profile/ModifyNickname.tsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import backButtonIcon from '@/assets/icon/guideDetailLeftArrow.svg';
 import MyProfile from './MyProfile';
+import Screen from '@/layouts/Screen';
+import TextField from '../common/TextField';
+import CTAButton from '../common/CTAButton';
 
 const myProfile = {
   nickname: '블루밍',
@@ -14,16 +17,18 @@ const ModifyNickname: React.FC = () => {
   const initialNickname = myProfile.nickname;
 
   return (
-    <div className="w-screen h-screen bg-Gray50">
-      <div className="flex justify-between px-[24px] pt-[31.31px]">
-        <img src={backButtonIcon} alt="뒤로가기 버튼 아이콘" />
-        <p className="text-[20px] text-Gray900 font-semibold">닉네임 수정</p>
-        <div className="w-[24px] h-[24px]" />
-      </div>
+    <Screen className="px-0">
+      <div className="bg-Gray50">
+        <div className="flex justify-between px-[24px] pt-[31.31px]">
+          <img src={backButtonIcon} alt="뒤로가기 버튼 아이콘" />
+          <p className="text-[20px] text-Gray900 font-semibold">닉네임 수정</p>
+          <div className="w-[24px] h-[24px]" />
+        </div>
 
-      <MyProfile justImg={true} myProfile={myProfile} />
-      <Nickname initialNickname={initialNickname} />
-    </div>
+        <MyProfile justImg={true} myProfile={myProfile} />
+        <Nickname initialNickname={initialNickname} />
+      </div>
+    </Screen>
   );
 };
 
@@ -33,30 +38,37 @@ interface NicknameProps {
 
 const Nickname: React.FC<NicknameProps> = ({ initialNickname }) => {
   const [nickname, setNickname] = useState<string>('');
-  const [isDefault, setIsDefault] = useState<boolean>(true);
 
-  useEffect(() => {
-    setNickname(initialNickname);
-  }, [initialNickname]);
+  const handleClear = () => {
+    setNickname('');
+  };
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setNickname(value);
-    setIsDefault(value === '');
   };
 
   return (
-    <>
-      <p className="pt-[30px] px-[24px] mb-[6px] text-Gray800 text-[13px] ">닉네임 수정</p>
-      <div className="box-border flex w-[calc(100%-40px)] mx-auto items-center justify-between text-[15px] font-medium">
-        <input
-          type="text"
-          value={nickname}
-          onChange={handleNicknameChange}
-          className={`px-[16px] py-[14px] w-full bg-white border border-GrayOpacity100  rounded-[10px] focus:outline-none ${isDefault ? 'text-Gray400' : 'text-Gray900'}`}
-        />
+    <div className="flex flex-col justify-between h-screen">
+      <div>
+        <p className="pt-[30px] px-[24px] mb-[6px] text-Gray800 text-[13px]">닉네임 수정</p>
+        <div className="box-border flex w-[calc(100%-40px)] mx-auto items-center justify-between text-[15px] font-medium">
+          <TextField
+            value={nickname}
+            onChange={handleNicknameChange}
+            description={`특수문자를 제외한 한글, 영어만 입력해주세요.(${nickname.length}/10)`}
+            className={`bg-white rounded-[10px] focus:outline-none w-full h-[54px]`}
+            white={true}
+            onClear={handleClear}
+            placeholder={initialNickname}
+          />
+        </div>
       </div>
-    </>
+
+      <div className="px-[24px] pb-[10px]">
+        <CTAButton text="확인" className="w-full bg-BloomingGreen500" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import MyProfile from '@/components/profile/MyProfile';
 import SetNotifications from '@/components/profile/SetNotifications';
 import TopButton from '@/components/profile/TopButton';
 import UseInformation from '@/components/profile/UseInformation';
+import Screen from '@/layouts/Screen';
 
 const Profile = () => {
   const myProfile = {
@@ -13,11 +14,13 @@ const Profile = () => {
   };
 
   return (
-    <div className="w-screen h-screen bg-Gray50">
-      <TopButton />
-      <MyProfile justImg={false} myProfile={myProfile} />
-      <SetNotifications myProfile={myProfile} />
-      <UseInformation />
+    <div className="bg-Gray50">
+      <Screen className="p-0">
+        <TopButton />
+        <MyProfile justImg={false} myProfile={myProfile} />
+        <SetNotifications myProfile={myProfile} />
+        <UseInformation />
+      </Screen>
     </div>
   );
 };
