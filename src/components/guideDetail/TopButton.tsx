081e495a9a688ac8fac6fb77f46b5bb225def741
박지윤 <tkrhkfld123@naver.com>
2024-08-19 00:59:19 +0900

    chore: 뒤로가기 구현

--- src/components/guideDetail/TopButton.tsx
+++ src/components/guideDetail/TopButton.tsx
@@ -1,10 +1,12 @@
 import backButtonIcon from '@/assets/icon/guideDetailLeftArrow.svg';
+import useInternalRouter from '@/hooks/useInternalRouter';
 
 const TopButton = () => {
+  const { goBack } = useInternalRouter();
   return (
     <>
       <div className="flex justify-between px-[24px] mt-[31.31px] mb-[15.5px]">
-        <img src={backButtonIcon} alt="뒤로가기 버튼 아이콘" />
+        <img src={backButtonIcon} alt="뒤로가기 버튼 아이콘" onClick={() => goBack()} />
       </div>
     </>
   );
