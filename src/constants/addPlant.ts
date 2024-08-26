ac522a4757b2333f0343107df562646ebd145e9e
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-26 16:18:22 +0900

    fix: simple scroll picker 값 1 ~ 6으로 변경

--- src/constants/addPlant.ts
+++ src/constants/addPlant.ts
@@ -1,23 +1,27 @@
 export const 마지막으로물비료준날선택값목록 = [
   {
     name: '오늘',
-    value: 0,
+    value: 1,
   },
   {
     name: '어제',
-    value: -1,
+    value: 2,
+  },
+  {
+    name: '3~4일 전',
+    value: 3,
   },
   {
     name: '일주일 전',
-    value: -7,
+    value: 4,
   },
   {
     name: '이주 혹은 그 이상 전',
-    value: -14,
+    value: 5,
   },
   {
     name: '잘 모르겠어요',
-    value: -100,
+    value: 6,
   },
 ];
 
