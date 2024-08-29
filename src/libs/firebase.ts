d250ee375267d10783726a5f0110e30db94c76f0
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-29 10:18:09 +0900

    feat: 파이어베이스 설정 추가

---
+++ src/libs/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const messaging = getMessaging(app);
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,32 @@
+import { getToken } from 'firebase/messaging';
+import { messaging } from '@/libs/firebase';
+
 import BloomingWeather from '@/components/main/BloomingWeather';
 import MyPlant from '@/components/main/MyPlant';
 import TabBar from '@/components/main/TabBar';
 import Screen from '@/layouts/Screen';
 import HeightBox from '@/components/common/HeightBox';
+import { useEffect } from 'react';
 
 const Main = () => {
   const register: boolean = true;
+
+  const requestPermission = async () => {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      const token = await getToken(messaging, {
+        vapidKey: import.meta.env.VITE_VAPID_KEY,
+      });
+      console.log(token);
+    } else {
+      console.log('Notification permission denied.');
+    }
+  };
+
+  useEffect(() => {
+    void requestPermission();
+  }, []);
+
   return (
     <Screen className="bg-Gray50 min-h-dvh">
       <MyPlant register={register} />
