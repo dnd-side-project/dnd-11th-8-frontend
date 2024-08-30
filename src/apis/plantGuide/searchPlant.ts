2a472fafda1562a8da5cefb210aef0af3c647efe
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-30 15:00:47 +0900

    fix: 식물 검색 페이지 로딩 UI, 에러 UI 수정

--- src/apis/plantGuide/searchPlant.ts
+++ src/apis/plantGuide/searchPlant.ts
@@ -6,6 +6,9 @@ export type SearchPlantResponse = {
 }[];
 
 export const searchPlant = async (query: string) => {
+  if (query === '') {
+    return [];
+  }
   const response = await privateAxios.get<SearchPlantResponse>(`/plants?plantName=${query}`);
   return response.data;
 };
