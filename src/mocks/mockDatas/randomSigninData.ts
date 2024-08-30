36032ebbba89d1d3e4a24d50f18e07b80047f497
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-08-30 11:13:41 +0900

    fix: 로그인 직후 요청에 헤더 포함 안되는 문제 수정

--- src/mocks/mockDatas/randomSigninData.ts
+++ src/mocks/mockDatas/randomSigninData.ts
@@ -3,6 +3,8 @@ export const randomSigninData = [
     status: 'success',
     accessToken: 'accessToken',
     refreshToken: 'refreshToken',
+    expiresIn: 21599,
+    refreshTokenExpiresIn: 2159999,
   },
   {
     status: 'pending',
