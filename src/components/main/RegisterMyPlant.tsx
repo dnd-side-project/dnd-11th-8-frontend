d784801e249b7a825c1b4c04a4bbf38b54308d38
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-09-30 14:38:08 +0900

    fix: whitespace pre wrap 적용한 후 스플릿 메서드 제거

--- src/components/main/RegisterMyPlant.tsx
+++ src/components/main/RegisterMyPlant.tsx
@@ -5,10 +5,8 @@ interface RegisterMyPlantProps {
 const RegisterMyPlant: React.FC<RegisterMyPlantProps> = ({ greetingMessage }) => {
   return (
     <div>
-      <div className="text-small-title text-Gray900 pt-[36.1px] pb-[30.3px]">
-        {greetingMessage.split('\n').map((line, index) => (
-          <p key={index}>{line}</p>
-        ))}
+      <div className="text-small-title text-Gray900 pt-[36.1px] pb-[30.3px] whitespace-pre-wrap">
+        <p>{greetingMessage}</p>
       </div>
     </div>
   );
