e9de82f98f1e82d0aa4e8f74be99d3595d88152b
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-09-30 18:01:55 +0900

    fix: 주소 검색 결과 없을시 UI 수정 및 선택 UI 추가

--- src/components/register/RegionList.tsx
+++ src/components/register/RegionList.tsx
@@ -2,13 +2,15 @@ import { useGetRegions } from '@/queries/useGetRegions.ts';
 import { regions } from '@/mocks/mockDatas/regions.ts';
 import { useCallback, useEffect, useState } from 'react';
 import { debounce } from 'es-toolkit';
+import { cn } from '@/utils.ts';
 
 interface RegionListProps {
   search: string;
+  selectedLocation: { id: number; name: string } | null;
   onSelectLocation: (location: (typeof regions)[number]) => void;
 }
 
-export const RegionList = ({ onSelectLocation, search }: RegionListProps) => {
+export const RegionList = ({ onSelectLocation, search, selectedLocation }: RegionListProps) => {
   const [deferredSearch, setSearch] = useState(search);
   const { data: locationList } = useGetRegions(deferredSearch);
 
@@ -24,9 +26,15 @@ export const RegionList = ({ onSelectLocation, search }: RegionListProps) => {
 
   return (
     <ul className={'h-full overflow-auto hide-scrollbar'}>
+      {locationList?.length === 0 && (
+        <li className={'py-4 text-regular-body font-medium text-Gray600'}>검색 결과가 없습니다.</li>
+      )}
       {locationList.map((location) => (
         <li
-          className={'py-4 text-regular-body text-Gray800 font-medium'}
+          className={cn(
+            'py-4 text-regular-body font-medium transition-colors',
+            selectedLocation?.id === location.id ? 'text-BloomingGreen500' : 'text-Gray800',
+          )}
           key={`searched-location-list-${location.id}`}
           onClick={() => onSelectLocation(location)}
         >
