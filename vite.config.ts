4f1f2a84b1ecfd46ff2e9b04b473ea088fcd0937
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-09-30 16:11:35 +0900

    feat: 메뉴얼 청킹 추가

--- vite.config.ts
+++ vite.config.ts
@@ -78,8 +78,14 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom', 'jotai', '@tanstack/react-query'],
+          react: ['react', 'react-dom'],
+          jotai: ['jotai'],
+          tanstack: ['@tanstack/react-query'],
           reactRouter: ['react-router-dom'],
+          fetch: ['axios'],
+          markdown: ['react-markdown'],
+          utils: ['es-toolkit'],
+          cookie: ['react-cookie'],
         },
       },
     },
