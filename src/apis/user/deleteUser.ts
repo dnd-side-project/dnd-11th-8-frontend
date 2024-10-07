e0c4f47f29ce977b7c2bafd7a6142c50aebd4cf1
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-07 19:05:17 +0900

    fix: 회원 탈퇴 안되던 문제 수정

--- src/apis/user/deleteUser.ts
+++ src/apis/user/deleteUser.ts
@@ -1,7 +1,7 @@
 import { privateAxios } from '@/libs/baseAxios.ts';
 import { deleteDeviceToken } from '@/apis/user/deleteDeviceToken.ts';
 
-const requestDeleteUser = () => privateAxios.post('/users/withdraw');
+const requestDeleteUser = () => privateAxios.delete('/users/withdraw');
 
 export const deleteUser = async () => {
   await deleteDeviceToken();
