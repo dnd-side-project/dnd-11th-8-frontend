774e0532c4ecc3a2ed673dc5cbd890f6b74a4d56
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-08 13:45:05 +0900

    fix: retry는 의도적으로 발생할 수 있도록 retry 되지 않도록 리액트 쿼리 기본 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -19,8 +19,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5,
-      retry: 3,
-      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      retry: false,
       throwOnError: true,
     },
   },
