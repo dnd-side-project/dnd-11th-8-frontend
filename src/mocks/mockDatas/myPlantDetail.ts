3d4593ea5a1687f01571974d79b9686b3db44dcf
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-31 19:46:06 +0900

    fix: 내 식물 상세 타입 으로 인한 배포 오류 수정

--- src/mocks/mockDatas/myPlantDetail.ts
+++ src/mocks/mockDatas/myPlantDetail.ts
@@ -18,6 +18,42 @@ export const myPlantDetail = {
   fertilizerAlarm: false,
   fertilizerPeriod: 30,
   healthCheckAlarm: false,
-  images: [],
+  images: [
+    {
+      imageId: 1,
+      imageUrl:
+        'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      favorite: true,
+      createdDate: '2024-05-20',
+    },
+    {
+      imageId: 2,
+      imageUrl:
+        'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      favorite: false,
+      createdDate: '2024-05-22',
+    },
+    {
+      imageId: 3,
+      imageUrl:
+        'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      favorite: false,
+      createdDate: '2024-05-23',
+    },
+    {
+      imageId: 4,
+      imageUrl:
+        'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      favorite: false,
+      createdDate: '2024-05-24',
+    },
+    {
+      imageId: 5,
+      imageUrl:
+        'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      favorite: false,
+      createdDate: '2024-05-25',
+    },
+  ],
 };
 export type MyPlantDetailType = typeof myPlantDetail;
