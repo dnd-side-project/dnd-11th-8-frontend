fb9b06aeeeba6b81f7e8fa360518f0e6eebcfe60
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-11-07 14:31:19 +0900

    fix: 타임아웃 기능 추가

--- src/libs/baseAxios.ts
+++ src/libs/baseAxios.ts
@@ -4,10 +4,12 @@ import { refreshAccessToken } from '@/apis/auth/refreshAccessToken.ts';
 import { setCookie } from '@/utils/cookie/setCookie.ts';
 
 export const baseAxios = axios.create({
+  timeout: 3000,
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const privateAxios = axios.create({
+  timeout: 3000,
   baseURL: import.meta.env.VITE_API_URL,
 });
 
